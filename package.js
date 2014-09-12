8b40616f829ae997d1ac08c1376b0f98ad205fe0
scorpwarp23 <shrenik.kenia@gmail.com>
2014-09-12 14:42:51 +0530

    version bumped

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Use IP address based whitelists and blacklists with your meteor app",
-  version: "1.0.0",
+  version: "1.0.1",
   git: "https://github.com/scorpwarp23/ip-blocker.git"
 });
 
